24b277d8008bf3236e9675eaeb0946d3ccb10fab
SDD <1058088225@qq.com>
2016-08-12 15:36:19 +0800

    加入判断密码功能

--- 4-手势解锁/4-手势解锁/NineView.m
+++ 4-手势解锁/4-手势解锁/NineView.m
@@ -8,9 +8,13 @@
 
 #import "NineView.h"
 #define kButtonCount 9
+#define kPassword @"123654789"
 @interface NineView()
 @property(nonatomic,strong)NSMutableArray *btns;//9个按钮
 @property(nonatomic,strong)NSMutableArray *lineBtns;//连线的按钮
+@property(nonatomic,assign)CGPoint currentPoint;//手指当前的位置
+
+
 @end
 @implementation NineView
 //懒加载
@@ -25,6 +29,9 @@
             [btn setBackgroundImage:[UIImage imageNamed:@"lock_btn_sel"] forState:UIControlStateHighlighted];//发现松手就变回原来的状态了
             //所以关闭用户交互，让其不可点击，用touchesMove来实现
             btn.userInteractionEnabled=NO;
+            //设置密码错误状态的图片
+            [btn setBackgroundImage:[UIImage imageNamed:@"lock_btn_error"] forState:UIControlStateSelected];
+            btn.tag=i+1;//设置按钮对应的号码，用于判断密码是否正确
             [self addSubview:btn];
             [self.btns addObject:btn];
         }
@@ -94,6 +101,7 @@
     UITouch *touch=touches.anyObject;
     //获取当前手指位置
     CGPoint p=[touch locationInView:touch.view];
+    self.currentPoint=p;//将手指的位置赋值给全局属性，用于连线
     for (int i=0; i<self.btns.count; i++) {
         UIButton *btn=self.btns[i];
         
@@ -125,16 +133,52 @@
             [path addLineToPoint:btn.center];
         }
     }
+    //判断连线的数组是不是有元素，如果不判断，当没有元素时会报一个没有起点的错误
+    if (self.lineBtns.count!=0) {
+        //往手指的位置连线
+        [path addLineToPoint:self.currentPoint];
+    }
+    
     //设置样式
     [[UIColor whiteColor]set];
     [path setLineWidth:10];
+    [path setLineCapStyle:kCGLineCapRound];//设置线条拐角处为圆角
+    [path setLineJoinStyle:kCGLineJoinRound];//设置连接处圆角
     //渲染
     [path stroke];
 }
 
 
 -(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self clear];
+    
+    NSString *password=@"";
+    for (int i=0; i<self.lineBtns.count; i++) {
+        UIButton *btn=self.lineBtns[i];
+        //NSLog(@"%ld",btn.tag);
+        password=[password stringByAppendingString:[NSString stringWithFormat:@"%ld",btn.tag]];//拼接密码
+    }
+    
+    NSLog(@"%@",password);
+    if(![password isEqualToString:kPassword]){
+        for (int i=0; i<self.lineBtns.count; i++) {
+            UIButton *btn=self.lineBtns[i];
+            btn.selected=YES;//如果没效果，就把highlighted改成NO，因为它俩冲突
+            btn.highlighted=NO;
+        }
+
+    }
+        //将当前点设置为lineBtn数组的最后一个元素的center,为了让超出部分的线松手后消失
+        self.currentPoint=[[self.lineBtns lastObject] center];
+        [self setNeedsDisplay];
+        //取消用户交互
+        self.userInteractionEnabled=NO;
+
+    //延时两秒再取消
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2* NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self clear];
+    });
+    
+
     
 }
 
@@ -143,7 +187,9 @@
     for (int i=0; i<self.btns.count; i++) {
         UIButton *btn=self.btns[i];
         btn.highlighted=NO;
+        btn.selected=NO;
     }
+    self.userInteractionEnabled=YES;
     //清空所有的画线数组
     [self.lineBtns removeAllObjects];
     //重绘
